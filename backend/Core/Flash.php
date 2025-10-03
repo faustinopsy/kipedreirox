ae7c9c92680f7b2c5bf91945e1dfa1aafb875874
faustinopsy <rodrigohipnose@gmail.com>
2025-10-03 10:42:00 -0300

    add redirect

--- backend/Core/Flash.php
+++ backend/Core/Flash.php
@@ -19,7 +19,7 @@ class Flash{
         }
         if (isset($_SESSION['flash'])) {
             $flash = $_SESSION['flash'];
-            //unset($_SESSION['flash']);
+            unset($_SESSION['flash']);
             return $flash;
         }
         return null;
